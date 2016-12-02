06d54cbb47b8ffa56fe4c61140dc00d33df3af9c
long <lphan1989@gmail.com>
2016-12-01 23:31:30 -0500

    meteor calls, no more database from client

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -44,27 +44,17 @@ class TodosListCtrl {
 
   addTask(newTask) {
     // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date,
-      user: Meteor.user()
-    });
-
+    Meteor.call('tasks.insert', newTask)
     // Clear form
     this.newTask = '';
   }
 
   setChecked(task) {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(task._id, {
-      $set: {
-        checked: !task.checked
-      },
-    });
+    Meteor.call('tasks.setChecked', task._id, !task.checked)
   }
 
   removeTask(task) {
-    Tasks.remove(task._id);
+    Meteor.call('tasks.remove', task._id)
   }
 }
 
